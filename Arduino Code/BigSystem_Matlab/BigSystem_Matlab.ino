e18722a5263f0a8e28ddba95b57d25f85b8e0a88
sitruklaq <44737287+sitruklaq@users.noreply.github.com>
2020-03-26 12:48:08 -0400

    added files

--- Arduino Code/BigSystem_Matlab/BigSystem_Matlab.ino
+++ Arduino Code/BigSystem_Matlab/BigSystem_Matlab.ino
@@ -21,23 +21,28 @@ float pressure;
 
 
 void setup() {
-  // put your setup code here, to run once:
+
   initializepins();
   Serial.begin(19200);
-
-}
-
-void loop() {
-  int n_pres_sensor = analogRead(A14);
+    int n_pres_sensor = analogRead(A14);
   int p_pres_sensor = analogRead(A15);
   int o_pres_sensor1 = analogRead(A13);
   int o_pres_sensor2=analogRead(A12);
   int o_pres_sensor3=analogRead(A11);
 int o_pres_sensor4=analogRead(A10);
 int o_pres_sensor5=analogRead(A9);
+
+}
+
+void loop() {
+
   pressurecontrol(n_pres_sensor, p_pres_sensor, o_pres_sensor1,o_pres_sensor2,o_pres_sensor3,o_pres_sensor4,o_pres_sensor5,pumppower);
-  unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis(); 
+ 
+  
+  
   recvWithStartEndMarkers();
+ 
  if (newData == true) {
         strcpy(tempChars, receivedChars);
             // this temporary copy is necessary to protect the original data
@@ -47,11 +52,5 @@ int o_pres_sensor5=analogRead(A9);
         newData = false;
  }
 
-  
-
-
-  if (p_switch) {
-    pressure_switch(pin, currentMillis, calltime);
-  }
 
 }
